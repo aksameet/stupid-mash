fedf96733f8fd2d4c502285df07eb43164b8b6a7
janusz.jaskowicz <jjaskowicz@kogifi.com>
2024-07-30 21:41:40 +0200

    nestjs connection

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from '@ionic/angular/standalone';
+import { DataService } from '../services/data.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, CommonModule],
 })
 export class HomePage {
-  constructor() {}
+  message: string = '';
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit() {
+    this.dataService.getData().subscribe((data) => {
+      this.message = data.message;
+    });
+  }
 }
